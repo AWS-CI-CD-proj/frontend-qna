f52e45e15b3836f81fc2a5df67815a452e5c516a
Rafe-kiho <tlsrlgh4783@yonsei.ac.kr>
2024-05-10 15:46:56 +0900

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button
+} from "@nextui-org/react";
+import NicknameButton from "./NicknameButton";
+
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar position="static">
+        <NavbarBrand className="flex flex-row gap-3">
+          <Link href="/">
+            <p className="font-bold text-inherit">Cloud School Q&A</p>
+          </Link>
+          <Button variant="bordered" color="primary">
+            <Link href="https://www.awsteam2.com">
+            출석부
+            </Link>
+          </Button>
+        </NavbarBrand>
+
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <NicknameButton />
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
